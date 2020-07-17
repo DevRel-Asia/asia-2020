94607a088c6accdf24ce15adab1fed1b57c994bf
Atsushi <atsushi@moongift.jp>
2020-07-17 18:04:28 +0900

    Add Raim

--- image.sh
+++ image.sh
@@ -23,3 +23,4 @@ mv /Users/nakatsugawa/Downloads/DevRelAsiaOGP/DevRelAsiaOGP.023.jpeg ./assets/im
 mv /Users/nakatsugawa/Downloads/DevRelAsiaOGP/DevRelAsiaOGP.024.jpeg ./assets/images/socials/odasho.jpg;
 mv /Users/nakatsugawa/Downloads/DevRelAsiaOGP/DevRelAsiaOGP.025.jpeg ./assets/images/socials/iganari.jpg;
 mv /Users/nakatsugawa/Downloads/DevRelAsiaOGP/DevRelAsiaOGP.026.jpeg ./assets/images/socials/eunoak.jpg;
+mv /Users/nakatsugawa/Downloads/DevRelAsiaOGP/DevRelAsiaOGP.027.jpeg ./assets/images/socials/raims.jpg;
